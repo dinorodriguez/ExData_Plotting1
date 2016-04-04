6558986992ae627ea8b09b0ea0abedac0f33688d
Dino Rodriguez <dino.rodriguez@gmail.com>
2016-04-04 10:34:42 -0500

    Plot1 Ok!

--- plot1.R
+++
@@ -1,5 +0,0 @@
-library(sqldf)
-gap <- read.csv.sql("../Documents/household_power_consumption.txt", sql = "select Global_active_power from file where Date = '1/2/2007' or Date = '2/2/2007'", header = TRUE, sep = ";")
-png("plot1.png", width = 480, height = 480)
-with(gap, hist(Global_active_power, col = 'red', main = "Global Active Power", xlab = "Global Active Power (kilowatts)"))
-dev.off()
